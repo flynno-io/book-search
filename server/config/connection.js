23cb4067204e9c0f30e1b296d19138158eed7b84
flynno <luke.laz.flynn@gmail.com>
2025-01-03 10:54:34 -0700

    updated db connection function and export

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
 const db = async () => {
   try {
