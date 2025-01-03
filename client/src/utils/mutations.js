0d9ff494fc6ae456947f65726bbf98ddaabb05d1
flynno <luke.laz.flynn@gmail.com>
2025-01-03 13:20:00 -0700

    added Apollo/Client packages to client code and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,62 @@
+import { gql } from "@apollo/client"
+
+export const LOGIN_USER = gql`
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`
+
+export const ADD_USER = gql`
+	mutation AddUser($userInput: UserInput!) {
+		addUser(userInput: $userInput) {
+			user {
+				_id
+				username
+				email
+			}
+			token
+		}
+	}
+`
+
+export const SAVE_BOOK = gql`
+	mutation saveBook($bookData: BookInput!) {
+		saveBook(bookData: $bookData) {
+			_id
+			username
+			email
+			savedBooks {
+				bookId
+				authors
+				description
+				title
+				image
+				link
+			}
+		}
+	}
+`
+
+export const REMOVE_BOOK = gql`
+	mutation removeBook($bookId: String!) {
+		removeBook(bookId: $bookId) {
+			_id
+			username
+			email
+			savedBooks {
+				bookId
+				authors
+				description
+				title
+				image
+				link
+			}
+		}
+	}
+`
