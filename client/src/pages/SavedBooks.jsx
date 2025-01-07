288c52312e52842ef0f2b7a29935a0bb6c488408
flynno <luke.laz.flynn@gmail.com>
2025-01-06 23:24:01 -0700

    fixed issue with GoogleAPI limits

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -18,7 +18,7 @@ const SavedBooks = () => {
 		const token = Auth.loggedIn() ? Auth.getToken() : null
 
 		if (!token) {
-      console.log("No token found!")
+			console.log("No token found!")
 			return false
 		}
 
@@ -51,7 +51,7 @@ const SavedBooks = () => {
 					{userData.savedBooks?.length
 						? `Viewing ${userData.savedBooks.length} saved ${
 								userData.savedBooks.length === 1 ? "book" : "books"
-              }:`
+						  }:`
 						: "You have no saved books!"}
 				</h2>
 				<Row>
